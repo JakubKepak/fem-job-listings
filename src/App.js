fb8b81c77fd15f09d27a76c5570c2819a287b483
JakubKepak <32369029+JakubKepak@users.noreply.github.com>
2021-02-23 16:41:18 +0100

    Base desktop layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+
+// images
+import backgroundHeaderImageDesktop from "./images/bg-header-desktop.svg";
+
+// components
+import ListingsItem from "./components/ListingsItem";
+
+// misc
+import { mockData } from "./mock/data";
+
+// themes
+const defaultTheme = {
+  backgroundColor: "hsl(180, 52%, 96%)",
+  companyNameTextColor: "hsl(180, 29%, 50%)",
+  positionNameTextColor: "hsl(180, 14%, 20%)",
+  newIconBackground: "hsl(180, 29%, 50%)",
+  featuredBackground: "hsl(180, 14%, 20%)",
+  whiteColor: "white",
+  lightGrey: "hsl(180, 8%, 52%)",
+};
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    background-color: ${({ theme }) => theme.backgroundColor};
+    display: flex;
+    justify-content: center;
+    height: 100vh;
+    font-family: 'Spartan', sans-serif;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const MainContainer = styled.div`
+  width: 100vw;
+  /* max-width: 1440px; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${({ theme }) => theme.backgroundColor};
+`;
+
+const Header = styled.div`
+  width: 100%;
+  height: 8rem;
+  background-image: url(${backgroundHeaderImageDesktop});
+  background-color: hsl(180, 29%, 50%);
+`;
+
+const ListingsContainer = styled.div`
+  width: 80%;
+  max-width: 1200px;
+  margin-top: 2rem;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <MainContainer>
+        <Header></Header>
+        <ListingsContainer>
+          {mockData.map((item) => {
+            return (
+              <ListingsItem
+                key={item.id}
+                company={item.company}
+                logo={require("" + item.logo)}
+                position={item.position}
+                featured={item.featured}
+                newItem={item.new}
+                postedAt={item.postedAt}
+                contract={item.contract}
+                location={item.location}
+                languages={item.languages}
+                tools={item.tools}
+              />
+            );
+          })}
+        </ListingsContainer>
+      </MainContainer>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/ListingsItem.js
@@ -0,0 +1,162 @@
+import styled from "styled-components";
+
+const ListingItemContainer = styled.div`
+  background-color: white;
+  padding: 1.5rem;
+  margin-bottom: 1.5rem;
+  box-shadow: 0px 5px 10px 1px rgba(0, 0, 0, 0.1);
+
+  display: flex;
+
+  ${({ featured }) =>
+    featured &&
+    `
+    border-left: 4px solid hsl(180, 29%, 50%);
+  `}
+`;
+
+const CompanyLogo = styled.img`
+  width: 4rem;
+  height: 4rem;
+`;
+
+const DescriptionContainer = styled.div`
+  margin-left: 1rem;
+  display: grid;
+  grid-auto-flow: row;
+  grid-gap: 0.5rem;
+`;
+
+const CompanyNameContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const CompanyName = styled.span`
+  font-size: 0.8rem;
+  font-weight: 700;
+  color: ${({ theme }) => theme.companyNameTextColor};
+  margin-right: 1rem;
+`;
+
+const NewIcon = styled.span`
+  background-color: ${({ theme }) => theme.newIconBackground};
+  text-transform: uppercase;
+  display: none;
+  color: ${({ theme }) => theme.whiteColor};
+  padding: 0.2rem 0.4rem 0.1rem 0.4rem;
+  border-radius: 20px;
+  margin-right: 0.4rem;
+  font-size: 0.7rem;
+  font-weight: 700;
+  text-align: center;
+
+  ${({ newItem }) =>
+    newItem &&
+    `
+    display: block;
+  `}
+`;
+
+const FeaturedIcon = styled.span`
+  background-color: ${({ theme }) => theme.featuredBackground};
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.whiteColor};
+  display: none;
+  padding: 0.2rem 0.4rem 0.1rem 0.4rem;
+  border-radius: 20px;
+  font-size: 0.7rem;
+  font-weight: 700;
+  text-align: center;
+
+  ${({ featured }) =>
+    featured &&
+    `
+    display: block;
+  `}
+`;
+
+const PositionName = styled.span`
+  color: ${({ theme }) => theme.positionNameTextColor};
+  font-weight: 700;
+`;
+
+const SpecificationContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Specification = styled.span`
+  color: ${({ theme }) => theme.lightGrey};
+  font-size: 0.8rem;
+`;
+
+const Dot = styled.span`
+  display: block;
+  width: 4px;
+  height: 4px;
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.lightGrey};
+  margin: 0 1rem;
+`;
+
+const ToolsContainer = styled.div`
+  margin-left: auto;
+  display: flex;
+  align-items: center;
+`;
+
+const ToolBox = styled.span`
+  padding: 0.4rem 0.6rem;
+  border-radius: 5px;
+  background-color: ${({ theme }) => theme.backgroundColor};
+  margin-left: 0.5rem;
+  color: ${({ theme }) => theme.companyNameTextColor};
+  font-weight: 700;
+  font-size: 0.7rem;
+
+  &:hover {
+    cursor: pointer;
+    background: ${({ theme }) => theme.companyNameTextColor};
+    color: ${({ theme }) => theme.whiteColor};
+  }
+`;
+
+export default function ListingsItem({
+  company,
+  logo,
+  featured,
+  newItem,
+  position,
+  postedAt,
+  contract,
+  location,
+  languages,
+  tools,
+}) {
+  return (
+    <ListingItemContainer featured={featured}>
+      <CompanyLogo src={logo.default} alt={company}></CompanyLogo>
+      <DescriptionContainer>
+        <CompanyNameContainer>
+          <CompanyName>{company}</CompanyName>
+          <NewIcon newItem={newItem}>New!</NewIcon>
+          <FeaturedIcon featured={featured}>featured</FeaturedIcon>
+        </CompanyNameContainer>
+        <PositionName>{position}</PositionName>
+        <SpecificationContainer>
+          <Specification>{postedAt}</Specification>
+          <Dot />
+          <Specification>{contract}</Specification>
+          <Dot />
+          <Specification>{location}</Specification>
+        </SpecificationContainer>
+      </DescriptionContainer>
+      <ToolsContainer>
+        {[...languages, ...tools].map((tool) => {
+          return <ToolBox>{tool}</ToolBox>;
+        })}
+      </ToolsContainer>
+    </ListingItemContainer>
+  );
+}
